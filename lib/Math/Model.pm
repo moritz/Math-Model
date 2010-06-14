28e20c062324b09637c2c1fe1818c9024d5d6f8b
Moritz Lenz <moritz@faui2k3.org>
2010-06-14 15:33:36 +0200

    move code to a module

---
+++ lib/Math/Model.pm
@@ -0,0 +1,59 @@
+use v6;
+
+module Math::Model;
+
+use Math::RungeKutta;
+
+sub model(:%derivatives, :%variables, :%initials) is export {
+    my %deriv-keying = %derivatives.keys Z=> 0..Inf;
+    my @derivs;
+    my @initial;
+    for %initials.pairs {
+        @initial[%deriv-keying{.key}] = .value;
+    }
+    for %derivatives.pairs {
+        @derivs[%deriv-keying{.key}]  = .value;
+    }
+
+    my sub param-names(&c) {
+        &c.signature.params».name».substr(1).grep: * !eq '_';
+    }
+
+    sub derivatives($time, @values) {
+        my sub params-for(&c) {
+            my %params;
+            for param-names(&c) -> $p {
+                my $value;
+                if $p eq 'time' {
+                    $value = $time;
+                } elsif %derivatives.exists($p) {
+                    $value = @values[%deriv-keying{$p}];
+                } elsif %variables.exists($p) {
+                    my $c = %variables{$p};
+                    $value = $c.(|params-for($c));
+                } else {
+                    die "Don't know where to get '$p' from.";
+                }
+                %params{$p} = $value;
+            }
+            return %params;
+        }
+        my @res = @values.keys.map: -> $i {
+            my $d      = @derivs[$i];
+            my %params = params-for($d);
+            $d(|%params);
+        };
+        @res;
+    }
+
+    adaptive-rk-integrate(
+        :from(0),
+        :to(10),
+        :@initial,
+        :derivative(&derivatives),
+        :max-stepsize(0.2),
+        :do(->$t, @v { say "$t\t@v[]"}),
+    );
+}
+
+# vim: ft=perl6
--- scratch.pl
+++ scratch.pl
@@ -1,10 +1,10 @@
 use v6;
-BEGIN { @*INC.push: '../Math-RungeKutta/lib' };
+BEGIN { @*INC.push: 'lib' };
 
-use Math::RungeKutta;
+use Math::Model;;
 
 
-my %model = {
+model(
     derivatives => {
         force       => { $:mass * $:velocity },
         velocity    => { $:height },
@@ -13,64 +13,9 @@ my %model = {
         mass        => { 10 },
         height      => { -$:force },
     },
-    initial     => {
+    initials    => {
         force       => 10,
         velocity    => 0,
     },
-};
+);
 
-
-{
-
-    my %deriv-keying = %model<derivatives>.keys Z=> 0..Inf;
-    my @derivs;
-    my @initial;
-    for %model<initial>.pairs {
-        @initial[%deriv-keying{.key}] = .value;
-    }
-    for %model<derivatives>.pairs {
-        @derivs[%deriv-keying{.key}]  = .value;
-    }
-
-    my sub param-names(&c) {
-        &c.signature.params».name».substr(1).grep: * !eq '_';
-    }
-
-    sub derivatives($time, @values) {
-        my sub params-for(&c) {
-            my %params;
-            for param-names(&c) -> $p {
-                my $value;
-                if $p eq 'time' {
-                    $value = $time;
-                } elsif %model<derivatives>.exists($p) {
-                    $value = @values[%deriv-keying{$p}];
-                } elsif %model<variables>.exists($p) {
-                    my $c = %model<variables>.{$p};
-                    $value = $c.(|params-for($c));
-                } else {
-                    die "Don't know where to get '$p' from.";
-                }
-                %params{$p} = $value;
-            }
-            return %params;
-        }
-        my @res = @values.keys.map: -> $i {
-            my $d      = @derivs[$i];
-            my %params = params-for($d);
-            $d(|%params);
-        };
-        @res;
-    }
-
-    adaptive-rk-integrate(
-        :from(0),
-        :to(10),
-        :@initial,
-        :derivative(&derivatives),
-        :max-stepsize(0.2),
-        :do(->$t, @v { say "$t\t@v[]"}),
-    );
-}
-
-# vim: ft=perl6
